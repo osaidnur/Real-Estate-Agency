9e6491ddcb42eea69139f087a57e9e9d53b90163
Osaid Nur <1210733@student.birzeit.edu>
2025-06-06 16:32:44 +0300

    Refactor: Update package names and app resources
    This commit refactors the project by:
    - Updating package names from `edu.birzeit.alproject` to `com.example.a1210733_1211088_courseproject`.
    - Changing the application name from "ALproject" to "Real Estate Agency".
    - Updating theme names in `themes.xml` and `AndroidManifest.xml`.
    - Modifying `MainActivity` to display `fragment_login` by default and commenting out window insets handling.
    - Renaming the home icon drawable from `ic_home.xml` to `ic_home1.xml` and updating its usage in layouts.

--- Android_Lab/app/src/main/java/com/example/a1210733_1211088_courseproject/LoginFragment.java
+++ Android_Lab/app/src/main/java/com/example/a1210733_1211088_courseproject/LoginFragment.java
@@ -1,4 +1,4 @@
-package edu.birzeit.alproject;
+package com.example.a1210733_1211088_courseproject;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.a1210733_1211088_courseproject.R;
 
 /**
  * A simple {@link Fragment} subclass.
--- Android_Lab/app/src/main/java/com/example/a1210733_1211088_courseproject/welcomeActivity.java
+++ Android_Lab/app/src/main/java/com/example/a1210733_1211088_courseproject/welcomeActivity.java
@@ -1,4 +1,4 @@
-package edu.birzeit.alproject;
+package com.example.a1210733_1211088_courseproject;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.a1210733_1211088_courseproject.R;
 
 public class welcomeActivity extends AppCompatActivity {
 
